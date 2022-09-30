f43cb26cc7ce87ad6bbc26b25409435b7de64fb3
Ben Randoing <benjamin.randoing@duke.edu>
2022-09-30 13:45:58 -0700

    Finished p2 with the exception of not knowing if rtesting procedure is correct

--- assign-1/02-Hailstone/Hailstone.js
+++ assign-1/02-Hailstone/Hailstone.js
@@ -2,12 +2,14 @@
  * File: Hailstone.js
  * ------------------
  * This program displays the Hailstone sequence for a number.
+ * Modified by Benjamin Randoing on 09/30/2022 for CS 106AX Autumn 2022
  */
 "use strict";
 
 function TestHailstone() {
    console.log("Use this expression evaluator to ensure your hailstone implementation works.");
 	evaluateExpressions();
+	hailstone(17);
 }
 
 /*
@@ -17,5 +19,20 @@ function TestHailstone() {
  * number down to 1 (along with information about how the intermediate numbers were computed).
  */
 function hailstone(n) {
-	console.log("This is a placeholder for the Hailstone sequence launched from the number " + n + ".");
+	let count = 0;
+	while (n != 1) {
+		if (n % 2 === 0) {
+			let n_old = n
+			n /= 2;
+			console.log(n_old + " is even, so I take half: " + n);
+			count += 1;
+		}
+		else {
+			let n_old = n
+			n = (3 * n) + 1;
+			console.log(n_old + " is odd, so I make 3n+1: " + n);
+			count += 1;
+		}
+	}
+	console.log("The process took " + count + " steps to reach 1.")
 }
